1246124fef93cf1beb93d547814874e25329f6d7
Rubén <ruben.sr.apple@gmail.com>
2024-03-26 11:09:41 +0100

    Iniciando proyecto weather en Xcode

---
+++ weather/weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Ruben on 26/3/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
